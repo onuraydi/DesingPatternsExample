5e1d4c3be8b57adc9baf0d621c5325454bb6add2
Onur Aydı <onuraydi@outlook.com>
2024-02-14 14:59:15 +0300

    Added Facade and Adapter desing patterns'

---
+++ Adapter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new XLogger());
+            productManager.Save();
+            ProductManager productManager2 = new ProductManager(new Log4netAdapter());
+
+            Console.WriteLine();
+
+            productManager2.Save();
+            Console.ReadLine();
+        }
+    }
+    class ProductManager
+    {
+        private ILogger _logger;
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("User Data!");
+            Console.WriteLine("Saved!");
+        }
+    }
+    interface ILogger
+    {
+        void Log(string message);
+    }
+    class XLogger:ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Logger, {0}",message);
+        }
+    }
+
+
+    // bu kısmın nuget'tan indirdiğimiz bir dll olduğunu varsaydık. Zaten Adatper Desing Pattern'i bu yüzden kullanılır.
+    class Log4Net
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logged wiht Log4Net, {0}", message);
+        }
+    }
+
+    class Log4netAdapter:ILogger
+    {
+        public void Log(string message)
+        {
+            Log4Net log4Net = new Log4Net();
+            log4Net.LogMessage(message);
+        }
+    }
+}
---
+++ Facat/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save();
+
+            Console.Read();
+        }
+    }
+    class Logging:ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged!");
+        }
+    }
+
+    public interface ILogging
+    { 
+        void Log(); 
+    }
+                   
+    class Caching:ICaching
+    {
+        public void Cached()
+        {
+            Console.WriteLine("Cached!");
+        }
+    }
+
+    public interface ICaching
+    {
+        void Cached();
+    }
+    class Authorize:IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("Used Checked!");
+        }
+    }
+    public interface IAuthorize
+    {
+        void CheckUser();
+    }
+    class CustomerManager
+    {
+        private CrossCuttingConcernsFacade _conserns;    
+        public CustomerManager()
+        {
+            _conserns = new CrossCuttingConcernsFacade();
+        }
+
+        public void Save()
+        {
+            _conserns.Logging.Log();
+            _conserns.Caching.Cached();
+            _conserns.Authorize.CheckUser();
+            Console.WriteLine("Saved!");
+        }
+    }
+
+    class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+
+        public CrossCuttingConcernsFacade()
+        {
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize();
+        }
+    }
+}
